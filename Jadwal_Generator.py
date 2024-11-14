7411436ad5c8354a925e7263a9f9bda31565f1c8
AryaDhrm <111678473+AryaDhrm@users.noreply.github.com>
2024-11-14 16:06:06 +0800

    Update Jadwal_Generator.py
    simpan dulu

--- Jadwal_Generator.py
+++ Jadwal_Generator.py
@@ -1,6 +1,5 @@
 import tkinter as tk
 from tkinter import messagebox,ttk
-# from tkinter import ttk
 from collections import defaultdict
 from PIL import Image, ImageDraw, ImageFont # type: ignore
 import textwrap
@@ -12,10 +11,10 @@ jadwal_Wfo_copy = {}
 
 
 def simpan_jadwal():
-    # load_jadwal()
     global anak_magang, jadwal_Wfo_copy
     
     input_anak_magang = input_nama.get()
+    anak_prioritas = input_anak_prioritas.get()
     min_wfh = input_max.get()
     max_wfo = input_max_wfo.get()
     anak_magang = [name.strip() for name in input_anak_magang.split(",")]
@@ -48,9 +47,9 @@ def simpan_jadwal():
     
     max_wfo_person = len(days) - min_wfh
     
-    print (f"ini adalah jumlah WFO setiap minggu {max_wfo_person}")
+    # print (f"ini adalah jumlah WFO setiap minggu {max_wfo_person}")
      
-    jumlah_wfh = {name: 0 for name in anak_magang}
+    jumlah_WFO = {name: 0 for name in anak_magang}
     max_jumlah_WFO = {day: max_wfo for day in days}
     
     # print(max_jumlah_WFO)
@@ -62,7 +61,7 @@ def simpan_jadwal():
     
     # max_loop = 0;
     
-    while any(jumlah < max_wfo_person for jumlah in jumlah_wfh.values()) and any(len(jadwal_Wfo[hari]) < max_wfo for hari in days):
+    while any(jumlah < max_wfo_person for jumlah in jumlah_WFO.values()) and any(len(jadwal_Wfo[hari]) < max_wfo for hari in days):
         hari = days[index_hari]
         nama = anak_magang[index_anak]
 
@@ -80,16 +79,16 @@ def simpan_jadwal():
                 nama = anak_magang[index_anak]
             
             # ini dirubah 
-        if  jumlah_wfh[nama] < max_wfo_person and nama not in jadwal_Wfo[hari]:
+        if  jumlah_WFO[nama] < max_wfo_person and nama not in jadwal_Wfo[hari]:
             # ini ditambahkan
             if len(jadwal_Wfo[hari]) < int(max_jumlah_WFO[hari]): 
                 jadwal_Wfo[hari].append(nama)
-                jumlah_wfh[nama] += 1
+                jumlah_WFO[nama] += 1
             else:
                 index_hari+=1
                 hari_sudah_diganti = True
                 jadwal_Wfo[hari].append(nama)
-                jumlah_wfh[nama] += 1
+                jumlah_WFO[nama] += 1
             # nama_ditambah = True
         
         if index_anak < total_magang - 1:
@@ -112,38 +111,28 @@ def simpan_jadwal():
         tree.delete(row)  
     
     for day in days:
-        # print(day)
-        # print(f"ini adalah : {day}")
         names = ', '.join(jadwal_Wfo[day])
-        # print(f"ini print  {jadwal_Wfo[day]}" )
         tree.insert("", tk.END, values=(day, names))
     
-    print(jumlah_wfh)
+    # print(jumlah_WFO)
     # jadwal_Wfo_copy = copy.deepcopy(jadwal_Wfo)
     copy_jadwal()
     jadwal_Wfo.clear() 
     reset_input()
-    
-    # update_dropdown()
-    # update_tabel(days)
-    # for day, names in jadwal_Wfo.items():
-    #     print(f"{day} : {names}")
-    
-    # print(jumlah_wfh)
+
 def copy_jadwal():
     try : 
         with open('jadwal_WFO.txt', 'r') as file:
             for line in file:
                 line = line.strip()
-                key, value = line.split(":")
-                jadwal_Wfo_copy[key] = value.split(",")
+                day, names = line.split(":")
+                jadwal_Wfo_copy[day] = names.split(",")
     except:
         pass
             
-    print(jadwal_Wfo_copy)
+    # print(jadwal_Wfo_copy)
 
 def reset_input():
-    """Reset input fields to empty."""
     input_nama.delete(0, tk.END)
     input_max.delete(0, tk.END)
     input_max_wfo.delete(0, tk.END)
@@ -151,22 +140,25 @@ def reset_input():
 def load_jadwal():
     global jadwal_Wfo, anak_magang
     
-    # Menghapus jadwal WFH yang ada
+    
     jadwal_Wfo.clear()
     
     try:
         # Membaca file jadwal WFH
         with open("jadwal_WFO.txt", "r") as file:
             lines = file.readlines()
-            
+            # print(f"ini adalah lines {lines}")
             # Memasukkan nama-nama anak magang dan jadwalnya ke dalam dictionary
             for line in lines:
                 # Mengambil nama hari dan nama-nama yang dijadwalkan
                 if ":" in line:
+                    # cek = 0
                     day, names = line.split(":")
                     names = names.strip()  # Menghapus spasi di awal dan akhir nama
-                    jadwal_Wfo[day.strip()] = names.split(", ")  # Memisahkan nama-nama yang dipisah koma
-
+                    jadwal_Wfo[day.strip()] = names.split(", ")
+               
+                    
+                    # Memisahkan nama-nama yang dipisah koma
         # Menampilkan kembali jadwal ke dalam Treeview
         for row in tree.get_children():
             tree.delete(row)
@@ -175,38 +167,16 @@ def load_jadwal():
             tree.insert("", tk.END, values=(day, ', '.join(names)))
 
         # Jika jadwal berhasil dimuat, memberikan info kepada user
-        messagebox.showinfo("Sukses", "Jadwal WFH berhasil dimuat.")
+        messagebox.showinfo("Sukses", "Jadwal WFO berhasil dimuat.")
         input_nama.focus_force()
 
     except FileNotFoundError:
         messagebox.showwarning("Anda belum punya jadwal", "Buat jadwal sekarang.")
-        print("Fokus dikembalikan ke input_nama")
+        # print("Fokus dikembalikan ke input_nama")
         input_nama.focus_force()
     except Exception as e:
         messagebox.showerror("Error", f"Terjadi kesalahan saat memuat jadwal: {e}")
         input_nama.focus_force()
-# Hapus nama
-'''
-# def hapus_nama():
-#     # days = days
-#     global jadwal_Wfo
-#     hapus_nama = nama_dropdown.get()
-    
-#     if not hapus_nama:
-#         messagebox.showerror("Pilih Nama", "Silakan pilih nama yang ingin dihapus dari dropdown.")
-#         return
-    
-#     for nama in jadwal_Wfo:
-#         if(hapus_nama in jadwal_Wfo[nama]):
-#             jadwal_Wfo[nama].remove(hapus_nama)
-    
-#     if hapus_nama in anak_magang:
-#         anak_magang.remove(hapus_nama)
-    
-#     update_dropdown()
-#     update_tabel(['senin', 'selasa', 'rabu', 'kamis', 'jumat', 'sabtu'])
-
-'''  
 
 
 def download_jadwal_as_image():
@@ -221,7 +191,7 @@ def download_jadwal_as_image():
     image_width = 1000
     margin = 40
     line_height = 80
-    max_text_width = image_width - (2 * margin + 150)  # Width limit for text wrapping
+    # max_text_width = image_width - (2 * margin + 150)  # Width limit for text wrapping
     total_height = margin * 2 + 60  # Initial height for title and top margin
      
     # Calculate required height based on text wrapping
@@ -285,35 +255,18 @@ def download_jadwal_as_image():
     image.save("jadwal_WFO.png")
     messagebox.showinfo("Sukses", "Jadwal WFO berhasil disimpan sebagai gambar (jadwal_WFO.png).")
 
-# function lain
-'''
-# def update_dropdown():
-#     nama_dropdown['values'] = anak_magang
-#     nama_dropdown.set("pilih nama")
-    
-# def update_tabel(days):
-#     for row in tree.get_children():
-#         tree.delete(row)
-        
-#     for day in days:
-#         # print(day)
-#         names = ', '.join(jadwal_WFH[day])
-#         tree.insert("", tk.END, values=(day, names))
-# # def regenerate_jadwal(){
-# #     simpan_jadwal()
-# # }
-# # simpan_jadwal()
-
-'''
 
 window = tk.Tk()
 window.title("Generator WFO")
 window.geometry("500x600")
-
 # Input orang
 tk.Label(window, text ="Nama anak PKL (pisahkan dengan koma) : ").pack(pady=10)
 input_nama = tk.Entry(window, width=50)
 input_nama.pack()
+
+tk.Label(window, text ="prioritas (pisahkan dengan koma) : ").pack(pady=20)
+input_anak_prioritas = tk.Entry(window, width=50)
+input_anak_prioritas.pack()
 
 tk.Label(window, text="Minimal WFH per-anak : ").pack(pady=10)
 input_max = tk.Entry(window, width= 50)
@@ -325,9 +278,6 @@ input_max_wfo.pack()
 
 save_button = tk.Button(window, text="Simpan Jadwal WFO", command=simpan_jadwal)
 save_button.pack(pady=20)
-
-# save_button = tk.Button(window, text="Load", command=load_jadwal)
-# save_button.pack(pady=20)
 
 download_button = tk.Button(window, text="Download Jadwal sebagai Gambar", command=download_jadwal_as_image)
 download_button.pack(pady=10)
@@ -343,11 +293,3 @@ tree.pack(expand=True, fill='both')
 copy_jadwal()
 load_jadwal()
 window.mainloop()
-# window.title("Penjadwalan WFH" )
-# window.geometry("400x300")
-# name_dropdown = ttk.Combobox(window, values=anak_magang)
-# name_dropdown.pack()
-# name_dropdown.set("Pilih nama")
-# window.mainloop()
-
-# hapus_nama()
